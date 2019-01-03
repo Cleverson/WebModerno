7be63332ddbb37e330e2f3798ceb6d6001f20df9
Cleverson <cleverson.s.dias@gmail.com>
2019-01-03 15:30:37 -0200

    Fundamentos-Objeto

---
+++ fundamentos/12_objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.9
+prod1['Desconto Legal'] = 0.4
+
+console.log(prod1)
+
+const prod2 = {
+  nome: 'Camisa polo',
+  preco: 79.8
+}
+
+console.log(prod2)
