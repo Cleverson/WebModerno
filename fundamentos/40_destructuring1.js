b37f75cf015aa65878db0fdb8a77eed3cc7d40ca
Cleverson <cleverson.s.dias@gmail.com>
2019-01-04 14:27:30 -0200

    Fundamentos-Destructuring1

---
+++ fundamentos/40_destructuring1.js
@@ -0,0 +1,29 @@
+// Novo recurso do ES2015
+
+const pessoa = {
+  nome: 'Cleverson',
+  idade: 44,
+  endereco: {
+    logradouro: 'Caminho do Paraiso',
+    numero: 326
+  }
+}
+
+const { nome, idade } = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade: i } = pessoa
+console.log(n, i)
+
+const { sobrenome, bemHumorado = true } = pessoa
+console.log(sobrenome, bemHumorado)
+
+const {
+  endereco: { logradouro, numero }
+} = pessoa
+console.log(logradouro, numero)
+
+const {
+  conta: { ag, num }
+} = pessoa
+console.log(ag, num)
