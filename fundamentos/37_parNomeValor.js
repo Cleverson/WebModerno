09e2b396fba2747042dacac2419d65e2c44eef03
Cleverson <cleverson.s.dias@gmail.com>
2019-01-04 11:24:19 -0200

    Fundamentos-ParNomeValor

---
+++ fundamentos/37_parNomeValor.js
@@ -0,0 +1,22 @@
+// Par nome/valor
+const saudacao = 'Opa' // Contexto léxico 1
+
+function exec() {
+  const saudacao = 'Falaaa' // Contexto léxico 2
+  return saudacao
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+  nome: 'Cleverson',
+  idade: 44,
+  peso: 90,
+  endereco: {
+    logradouro: 'Estrada do Paraiso',
+    numero: 326
+  }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
