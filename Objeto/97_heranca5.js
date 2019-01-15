3999b94077cef8fe5907cf2ed2ccc243518fae00
Cleverson <cleverson.s.dias@gmail.com>
2019-01-15 15:56:35 -0200

    Objeto-Heranca5

---
+++ Objeto/97_heranca5.js
@@ -0,0 +1,25 @@
+console.log(typeof String)
+console.log(typeof Array)
+console.log(typeof Object)
+
+String.prototype.reverse = function() {
+  return this.split('')
+    .reverse()
+    .join('')
+}
+
+console.log('Escola Cod3r'.reverse())
+
+Array.prototype.first = function() {
+  return this[0]
+}
+
+console.log([1, 2, 3].first())
+console.log(['a', 'b', 'c'].first())
+
+// Subscrevendo toString - não fazer isso
+String.prototype.toString = function() {
+  return 'Lascou Tudo'
+}
+
+console.log('Escola Cod3e'.reverse())
