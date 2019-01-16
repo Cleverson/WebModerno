8ce7a6fcc7a35a8a2bc4d2f86a7725d9e6681ef8
Cleverson <cleverson.s.dias@gmail.com>
2019-01-16 15:36:54 -0200

    Node

---
+++ Node/120_moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./120_moduloA')
+const moduloB = require('./120_moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.benVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
