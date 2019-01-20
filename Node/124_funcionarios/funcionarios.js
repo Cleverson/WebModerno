1763cbe3a5f1dd8a5489e3925bd49962eb3734f5
Cleverson <cleverson.s.dias@gmail.com>
2019-01-20 13:01:36 -0200

    Esnext

--- Node/124_funcionarios/funcionarios.js
+++ Node/124_funcionarios/funcionarios.js
@@ -4,18 +4,18 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const chineses = f => f.pais === 'China'
 const mulheres = f => f.genero === 'F'
 const menorSalario = (func, funcAtual) => {
-    return func.salario > funcAtual.salario ? func : funcAtual
+  return func.salario < funcAtual.salario ? func : funcAtual
 }
 
 axios.get(url).then(response => {
-    const funcionarios = response.data
-    // console.log(funcionarios)
+  const funcionarios = response.data
+  // console.log(funcionarios)
 
-    // Mulheres Chinesas com o menor salário
-    const func = funcionarios
-        .filter(chineses)
-        .filter(mulheres)
-        .reduce(menorSalario)
+  // Mulheres Chinesas com o menor salário
+  const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario)
 
-    console.log(func)
+  console.log(func)
 })
