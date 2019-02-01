c08936fd1dcf7912b205037f4988f826139962aa
Cleverson <cleverson.s.dias@gmail.com>
2019-02-01 16:09:01 -0200

    Ajax

--- Ajax/252_server.js
+++ Ajax/252_server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const multer = require('multer')
 const app = express()
 
 app.use(express.static('.'))
@@ -8,6 +9,26 @@ app.use(bodyParser.json())
 
 app.get('/teste', (req, res) => {
     res.send('OK')
+})
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro!')
+        }
+        res.end('Concluído com sucesso')
+    })
 })
 
 app.listen(8080, () => console.log('Executando...'))
