bd897f53856af9b9cc8ca80afb0eca2acb39cca8
cleverson <cleverson.s.dias@gmail.com>
2019-02-26 16:17:18 -0300

    Exercícios Express

---
+++ exercicios-express/api/produto.js
@@ -0,0 +1,13 @@
+module.exports = (app, texto) => {
+    function salvar(req, res) {
+        res.send(`Produto > salvar > ${texto}`)
+    }
+
+    function obter(req, res) {
+        res.send(`Produto > obter > ${texto}`)
+    }
+    app.post('/produtos', salvar)
+    app.get('/produtos', obter)
+
+    return { salvar, obter }
+}
